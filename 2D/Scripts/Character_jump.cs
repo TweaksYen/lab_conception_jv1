8856264a648e91814873fc2cfcaafac1c94de7da
Oni0nKnight <theophile.gindre@gmail.com>
2016-09-28 20:30:11 -0400

    commit with assets

---
+++ 2D/Scripts/Character_jump.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character_jump : MonoBehaviour {
+	// **** jump characteristics ****
+	[SerializeField] public float jumpForceX = 400f;	// Amount of force added when the player jumps.	
+	[SerializeField] public float jumpForceY = 500f;	// Amount of force added when the player jumps.	
+	[SerializeField] public float jumpTime = .7f;		// duration of the normal jump
+	[SerializeField] public float wallJumpTime = .6f;	// duration of the walljump
+	[Range(0, 1)]
+	[SerializeField] public float airControl = .75f;			// Amount of maxSpeed applied to in air movement. 1 = 100%
+	bool jumping = false;
+
+	// **** multiple jumps ****
+	[SerializeField] int jumps_limit = 2;
+	int jumps_left = 2;
+
+	// **** walljumps ****
+	Transform wallCheck;
+	[SerializeField] public bool walled;
+	float walledRadius = .05f;							// Radius of the overlap circle to determine if walled
+
+	private PlatformerCharacter2D character;
+
+
+	void Awake() {
+		character = GetComponent<PlatformerCharacter2D>();
+		wallCheck = transform.Find ("WallCheck");
+	}
+
+	void FixedUpdate() {
+		//boolean for walljumps
+		walled = Physics2D.OverlapCircle(wallCheck.position, walledRadius, character.whatIsGround);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (character.grounded && !jumping) {
+			jumps_left = jumps_limit; //reset jump limit
+		}
+	}
+
+	public void jump() {
+		StartCoroutine (JumpRoutine ());
+	}
+
+	public bool canJump() {
+		return (jumps_left > 0 || walled);
+	}
+
+
+	IEnumerator JumpRoutine()
+	{
+		GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
+
+		jumping = true;
+
+		if (walled && !character.grounded) { // *** walljump ***
+			GetComponent<Rigidbody2D> ().AddForce(new Vector2 (jumpForceX * (character.facingRight ? -1 : 1), jumpForceY));
+			character.Flip ();
+			character.move_allowed = false;
+
+			float timer = 0;
+			while (timer < wallJumpTime)
+			{
+				timer += Time.deltaTime;
+				yield return null;
+			}
+
+			character.move_allowed = true;
+		} else { // *** normal jump ***
+			jumps_left--;
+			character.move_allowed = true;
+
+			//calcul de la hauteur de saut (non fonctionnel
+			float n = (jumpTime / Time.fixedDeltaTime) - 1;
+			float r = -20*(Time.fixedDeltaTime / jumpTime);
+			float u0 = 20 * (1 - (Time.fixedDeltaTime / jumpTime));
+			float max_force = jumpForceY + (n+1)*(2*u0 + n*r)/2;
+			float hauteur_saut = (max_force * jumpTime * jumpTime) / GetComponent<Rigidbody2D> ().mass;
+			Debug.Log ("a priori : " + hauteur_saut);
+
+			float timer = 0;
+			while (CrossPlatformInput.GetButton("Jump") && timer < jumpTime)
+			{
+				float proportionCompleted = timer / jumpTime;
+				Vector2 thisFrameJumpVector;
+
+				if (timer == 0)
+					thisFrameJumpVector = new Vector2 (0f, jumpForceY);
+				else
+					thisFrameJumpVector = Vector2.Lerp(new Vector2 (0f, 20f), Vector2.zero, proportionCompleted);
+				max_force += thisFrameJumpVector.y;
+
+				GetComponent<Rigidbody2D> ().AddForce(thisFrameJumpVector);
+				timer += Time.fixedDeltaTime;
+				yield return null;
+			}
+		}
+
+		jumping = false;
+	}
+}
---
+++ 2D/Scripts/PlatformerCharacter2D.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformerCharacter2D : MonoBehaviour 
+{
+	// **** general ****
+	public bool facingRight = true;							// For determining which way the player is currently facing.
+	public bool move_allowed = true;							// allowed to move
+	[SerializeField] float maxSpeed = 10f;				// The fastest the player can travel in the x axis.
+	[Range(0, 1)]
+	[SerializeField] float crouchSpeed = .25f;			// Amount of maxSpeed applied to crouching movement. 1 = 100%
+
+	// **** other ****
+	[SerializeField] public LayerMask whatIsGround;			// A mask determining what is ground to the character
+	Transform groundCheck;								// A position marking where to check if the player is grounded.
+	float groundedRadius = .1f;							// Radius of the overlap circle to determine if grounded
+	public bool grounded = false;						// Whether or not the player is grounded.
+	Transform ceilingCheck;								// A position marking where to check for ceilings
+	float ceilingRadius = .01f;							// Radius of the overlap circle to determine if the player can stand up
+	Animator anim;										// Reference to the player's animator component.
+
+	private Character_blink blinker;
+	private Character_jump jumper;
+
+    void Awake()
+	{
+		// Setting up references.
+		groundCheck = transform.Find("GroundCheck");
+		ceilingCheck = transform.Find("CeilingCheck");
+
+		anim = GetComponent<Animator>();
+		blinker = GetComponent<Character_blink>();
+		jumper = GetComponent<Character_jump> ();
+	}
+
+
+	void FixedUpdate()
+	{
+		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundedRadius, whatIsGround);
+		anim.SetBool("Ground", grounded);
+
+		// Set the vertical animation
+		anim.SetFloat("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
+	}
+
+
+	public void Move(float move, bool crouch, bool jumpButtonPressed, bool blinkButtonPressed)
+	{
+		/***** Crounch handling *****/
+		// If crouching, check to see if the character can stand up
+		if(!crouch && anim.GetBool("Crouch"))
+		{
+			// If the character has a ceiling preventing them from standing up, keep them crouching
+			if( Physics2D.OverlapCircle(ceilingCheck.position, ceilingRadius, whatIsGround))
+				crouch = true;
+		}
+		anim.SetBool("Crouch", crouch);
+
+		/***** x-Deplacement system *****/
+		if (move_allowed) {
+			//only control the player if grounded or airControl is turned on
+			if (grounded) {
+				// Reduce the speed if crouching by the crouchSpeed multiplier
+				move = (crouch ? move * crouchSpeed : move);
+			} else {
+				move = move * jumper.airControl;
+			}
+
+			// The Speed animator parameter is set to the absolute value of the horizontal input.
+			anim.SetFloat ("Speed", Mathf.Abs (move));
+
+			if (move != 0){
+				// Move the character
+				GetComponent<Rigidbody2D> ().velocity = new Vector2 (move * maxSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+
+				//flip the character if needed
+				if (move > 0 && !facingRight)
+					Flip ();
+				else if (move < 0 && facingRight)
+					Flip ();
+			}
+		}
+
+		/***** jump handling *****/
+		if (jumpButtonPressed && jumper.canJump()) {
+			anim.SetBool("Ground", false);
+			jumper.jump();
+        }
+
+		/***** blink handling *****/
+		if (blinkButtonPressed && blinker.can_blink) {
+			blinker.blink();
+		}
+	}
+
+	
+	public void Flip ()
+	{
+		// Switch the way the player is labelled as facing.
+		facingRight = !facingRight;
+		
+		// Multiply the player's x local scale by -1.
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+		blinker.flip_ghost (theScale);
+	}
+		
+}
